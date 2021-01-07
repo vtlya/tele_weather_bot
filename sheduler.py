0ae46897a10546eab914b2619c5540624cb0aa49
vtlya <56250551+vtlya@users.noreply.github.com>
2021-01-07 19:20:54 +0300

    bug fix with double message

--- sheduler.py
+++ sheduler.py
@@ -44,17 +44,9 @@ def msg(apikey, time):
                 except:
                     #bot.send_message(user, weather, parse_mode='Markdown')  # делаем рассылку погоды каждому
                     print('Не отправилось письмо! Что-то с телеграм либой походу :(')
-                bot.send_message(user, weather, parse_mode='Markdown')  # делаем рассылку погоды каждому
     except:
         pass
 
-#шедулим отправку для времен с двузначным значением
-times2 = ["10:00", "11:00", "12:00", "13:00",
-          "15:45",
-          "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00"]
-for hour in times2:
-    schedule.every().day.at(str(hour)).do(msg, apikey, hour)
-    print('sheduled at '+ hour)
 
 #шедклим отправку для времен с однозначным значением
 times1 = [ "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00"]
@@ -62,6 +54,12 @@ for hour in times1:
     h2 = '0'+str(hour)
     schedule.every().day.at(str(h2)).do(msg, apikey, hour)
     print('sheduled at ' + hour)
+
+#шедулим отправку для времен с двузначным значением
+times2 = ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00"]
+for hour in times2:
+    schedule.every().day.at(str(hour)).do(msg, apikey, hour)
+    print('sheduled at '+ hour)
 
 while True:
     schedule.run_pending()
